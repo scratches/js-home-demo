79712c9e30532ae3291d5151dd82782b2a330f2f
Dave Syer <dsyer@vmware.com>
2023-11-24 09:03:47 +0000

    Use content negotiation strategy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,40 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		HeaderContentNegotiationStrategy strategy = new HeaderContentNegotiationStrategy() {
+			@Override
+			public List<MediaType> resolveMediaTypes(NativeWebRequest request) throws HttpMediaTypeNotAcceptableException {
+				List<MediaType> mediaTypes = super.resolveMediaTypes(request);
+				if (mediaTypes.contains(MediaType.TEXT_HTML)) {
+					mediaTypes.remove(MediaType.ALL);
+				}
+				return mediaTypes;
+			}
+		};
+		configurer.strategies(List.of(strategy));
 	}
 
 }
